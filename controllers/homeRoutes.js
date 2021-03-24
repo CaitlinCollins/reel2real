f55fc1cd5f86c8218ad7e104cd8c9b5abf643677
Caitlin Collins <caitlin.aileen86@gmail.com>
2021-03-23 21:36:15 -0500

    Created main and homepage.handlebars

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,19 +2,16 @@ const router = require('express').Router();
 const { User } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', withAuth, async (req, res) => {
+router.get('/', async (req, res) => {
   try {
-    const userData = await User.findAll({
-      attributes: { exclude: ['password'] },
-      order: [['name', 'ASC']],
-    });
+    // const userData = await User.findAll({
+    //   attributes: { exclude: ['password'] },
+    //   order: [['name', 'ASC']],
+    // });
 
-    const users = userData.map((project) => project.get({ plain: true }));
+    // const users = userData.map((project) => project.get({ plain: true }));
 
-    res.render('homepage', {
-      users,
-      logged_in: req.session.logged_in,
-    });
+    res.render('homepage');
   } catch (err) {
     res.status(500).json(err);
   }
