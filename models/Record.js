ea480ef3d943e2d84c36f8c2acdc7fc746a549fc
ChrisBoisjoli <christopherboisjoli@gmail.com>
2021-03-25 20:06:05 -0500

    updated index and record for has many and belongs

--- models/Record.js
+++ models/Record.js
@@ -41,6 +41,13 @@ Record.init(
       type: DataTypes.STRING,
       allowNull: false,
     },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+          model: 'user',
+          key: 'id'
+      }
+  },
   },
   {
     sequelize,
--- models/index.js
+++ models/index.js
@@ -2,6 +2,16 @@ const User = require('./User');
 
 const Record = require('./Record');
 
+Record.belongsTo(User, {
+    foreignKey: 'user_id',
+    onDelete: 'CASCADE'
+});
+
+User.hasMany(Record, {
+    foreignKey: 'user_id',
+});
+
+
 module.exports = { User, Record };
 
 
