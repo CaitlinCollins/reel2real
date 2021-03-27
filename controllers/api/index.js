96dd700244b9810a8c0a9ab46d31b641c9c8d966
Frank Merchlewitz <jlimhb@gmail.com>
2021-03-27 11:30:09 -0500

    Moved Searched Routes into API

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
 const crateRoutes = require('./crateRoutes');
+const searchRoutes = require('./searchRoutes');
+
 
 router.use('/users', userRoutes);
 router.use('/crate', crateRoutes);
+router.use('/search', searchRoutes);
 
 
 module.exports = router;
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -26,4 +26,14 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
+router.get('/search',  async (req, res) => {
+  try {
+      res.render('search', {
+          logged_in: req.session.logged_in,
+        });
+  }catch (err){
+      res.status(500).json(err);
+    }
+});
+
 module.exports = router;
