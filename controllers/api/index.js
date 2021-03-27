24e844e57ba57dc3d024d97bcd24367e2ef59ed3
ChrisBoisjoli <christopherboisjoli@gmail.com>
2021-03-27 11:03:27 -0500

    created routes for crates

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,9 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const crateRoutes = require('./crateRoutes');
 
 router.use('/users', userRoutes);
-router.use('/crate', crateRoutes);
-
 
 module.exports = router;
