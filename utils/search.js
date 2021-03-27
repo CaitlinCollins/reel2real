da942f287198ff146b734d050140587d61db32a7
Frank Merchlewitz <jlimhb@gmail.com>
2021-03-27 10:24:28 -0500

    api key update

--- utils/search.js
+++ utils/search.js
@@ -1,9 +1,11 @@
 // var Discogs = require('disconnect').Client;
 const fetch = require("node-fetch");
 
+var apiKey = process.env.API_KEY
+var apiSecret = process.env.API_SECRET
 var genre = "jazz";
 var artist = "Stanley Clarke";
-var queryUrl = `https://api.discogs.com/database/search?key=nvWMvkXWSygfbACCOVQc&secret=mDVWMOCUTByoFszZgQyjUBnYcYbjnkYB&genre=${genre}&artist=${artist}&per_page=15&page=1`
+var queryUrl = `https://api.discogs.com/database/search?key=${apiKey}&secret=${apiSecret}&genre=${genre}&artist=${artist}&per_page=15&page=1`
 // var imageQueryURL = "https://api.discogs.com/masters/{id}?key=nvWMvkXWSygfbACCOVQc&secret=mDVWMOCUTByoFszZgQyjUBnYcYbjnkYB"
 
 
