de47219ea18f1323132d18a8f98e4180dfa9952b
Caitlin Collins <caitlin.aileen86@gmail.com>
2021-03-28 11:59:00 -0500

    Fixed search route

--- public/js/login.js
+++ public/js/login.js
@@ -1,12 +1,10 @@
 const loginFormHandler = async (event) => {
   event.preventDefault();
 
-
   const email = document.querySelector('#email-login').value.trim();
   const password = document.querySelector('#password-login').value.trim();
 
   if (email && password) {
-
     const response = await fetch('/api/users/login', {
       method: 'POST',
       body: JSON.stringify({ email, password }),
